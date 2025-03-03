6f1cbc59d43654803d180d9ccc3229b3f646b463
Tarun Dhillon <tarundhillon14330@gmail.com>
2025-03-03 11:38:23 +0530

    customize radix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,15 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Theme } from "@radix-ui/themes";
+import { Inter } from "next/font/google";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -28,12 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={inter.variable}>
         <Theme>
           <NavBar/>
-          <main className="p-5">
-            {children}
-          </main>
+          <main className="p-5"> {children} </main>
         </Theme>
       </body>
     </html>

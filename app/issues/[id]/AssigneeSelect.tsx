c2272f6399ab753eb86ba5e969be30097b58f39a
Tarun Dhillon <tarundhillon14330@gmail.com>
2025-03-08 23:39:43 +0530

    Assign an issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,41 +1,52 @@
-'use client'
+"use client";
 
-import { User } from '@prisma/client'
-import { Select } from '@radix-ui/themes'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useQuery } from '@tanstack/react-query'
-import { Skeleton} from '@/app/components'
+import { Issue, User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@/app/components";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
+  });
 
-    const {data: users, error, isLoading} = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then(res => res.data),
-        staleTime: 60*1000,
-        retry: 3
-    })
+  if (isLoading) {
+    return <Skeleton />;
+  }
 
-    if(isLoading){
-        return <Skeleton/>
-    }
+  if (error) return null;
 
-    if(error) return null
-    
-    
   return (
-    <Select.Root>
-        <Select.Trigger placeholder='Assign...' />
-        <Select.Content>
-            <Select.Group>
-                <Select.Label>Suggestions</Select.Label>
-                {users?.map(user => (
-                    <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
-                ))}
-            </Select.Group>
-        </Select.Content>
+    <Select.Root 
+    defaultValue={issue.assignedToUserId || "0"}
+      onValueChange={(userId) => {
+        const assignedToUserId = userId === "0" ? null : userId;
+        axios.patch("/api/issues/" + issue.id, { assignedToUserId });
+      }}
+    >
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          <Select.Item value="0">Unassigned</Select.Item>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
